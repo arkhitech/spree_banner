5211cd35169ee6e9f2d9eaa1f65ec2f25a681b1d
Iqra Rastee <iqra.rastee@arkhitech.com>
2014-10-27 11:11:33 +0500

    removed link to from banner

--- app/helpers/spree/banner_boxes_helper.rb
+++ app/helpers/spree/banner_boxes_helper.rb
@@ -13,20 +13,20 @@ module Spree
         content_tag :ul do
           banners.map do |ban|
             content_tag :li, :class => params[:class] do
-              link_to (ban.url.blank? ? "javascript: void(0)" : ban.url) do
-                src = ban.attachment.url(params[:style].to_sym)
-                image_tag(src, :alt => ban.alt_text.presence || image_alt(src))
-              end
+#              link_to (ban.url.blank? ? "javascript: void(0)" : ban.url) do
+#                src = ban.attachment.url(params[:style].to_sym)
+#                image_tag(src, :alt => ban.alt_text.presence || image_alt(src))
+#              end
             end
           end.join.html_safe
         end
       else
         banners.map do |ban|
           content_tag :div, :class => params[:class] do
-            link_to (ban.url.blank? ? "javascript: void(0)" : ban.url) do
+            #link_to (ban.url.blank? ? "javascript: void(0)" : ban.url) do
               src = ban.attachment.url(params[:style].to_sym)
               image_tag(ban.attachment.url(params[:style].to_sym), :alt => ban.alt_text.presence || image_alt(src))
-            end
+            #end
           end
         end.join.html_safe
       end
