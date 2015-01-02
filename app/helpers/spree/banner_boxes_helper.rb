70ed95d7c6c65baea1993071480e945812f8ebb4
Mudasir Raza <mudasir.raza@arkhitech.com>
2015-01-02 17:07:18 +0500

    conditionally show link on banner

--- app/helpers/spree/banner_boxes_helper.rb
+++ app/helpers/spree/banner_boxes_helper.rb
@@ -13,20 +13,29 @@ module Spree
         content_tag :ul do
           banners.map do |ban|
             content_tag :li, :class => params[:class] do
-#              link_to (ban.url.blank? ? "javascript: void(0)" : ban.url) do
-#                src = ban.attachment.url(params[:style].to_sym)
-#                image_tag(src, :alt => ban.alt_text.presence || image_alt(src))
-#              end
+              unless ban.url.blank?
+                link_to (ban.url.blank? ? "javascript: void(0)" : ban.url) do
+                  src = ban.attachment.url(params[:style].to_sym)
+                  image_tag(src, :alt => ban.alt_text.presence || image_alt(src))
+                end
+              else
+                
+              end
             end
           end.join.html_safe
         end
       else
         banners.map do |ban|
           content_tag :div, :class => params[:class] do
-            #link_to (ban.url.blank? ? "javascript: void(0)" : ban.url) do
+            unless ban.url.blank?
+              link_to (ban.url.blank? ? "javascript: void(0)" : ban.url) do
+                src = ban.attachment.url(params[:style].to_sym)
+                image_tag(ban.attachment.url(params[:style].to_sym), :alt => ban.alt_text.presence || image_alt(src))
+              end
+            else
               src = ban.attachment.url(params[:style].to_sym)
               image_tag(ban.attachment.url(params[:style].to_sym), :alt => ban.alt_text.presence || image_alt(src))
-            #end
+            end
           end
         end.join.html_safe
       end
